ea989de6d72cff38605077e0ef5fbe1b03f6e11d
Jetmate <antonoutkine@gmail.com>
2018-01-06 19:08:57 -0600

    Fix some errors

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 const fetch = require('node-fetch')
 const parseString = require('xml2js').parseString
 const stringify = require('querystring').stringify
+const fs = require('fs')
 
-const boardGames = ['Blokus', 'Blokus 3D', 'Agricola']
-let ids = []
-
-for (const game of boardGames) {
-  searchId(game, id => {
-    ratingFromId(id)
+const boardGamesCSV = fs.readFileSync('./board_games.csv')
+const boardGames = boardGamesCSV
+  .toString()
+  .split('\n')
+  .filter(line => line)
+  .map(line => {
+    return line.split(',')[0]
   })
+
+const ratings = []
+
+for (let i = 0; i < boardGames.length; i++) {
+  setTimeout(() => {
+    console.log(boardGames[i])
+    searchId(boardGames[i], id => {
+      ratingFromId(id, rating => {
+        ratings.push(rating)
+        if (i === boardGames.length - 1) {
+          fs.writeFileSync('./board_games.csv', boardGames
+            .map((game, i) => [game, ratings[i]].join(','))
+            .join('\n')
+          )
+        }
+      })
+    })
+  }, i * 3000)
 }
 
 function get (endpoint, parameters, callback) {
@@ -25,13 +45,17 @@ function get (endpoint, parameters, callback) {
 
 function searchId (name, callback) {
   get('search', { query: name, type: 'boardgame' }, result => {
-    if (!result.items.item.length) throw new Error('No results for ' + name)
-    callback(result.items.item[0].$.id)
+    if (!result.items || !result.items.item || !result.items.item.length) callback('error')
+    else callback(result.items.item[0].$.id)
   })
 }
 
 function ratingFromId (id, callback) {
-  get('thing', { id: id, type: 'boardgame', stats: 2 }, result => {
-    console.log(result.items.item[0].statistics[0].ratings[0].average[0].$.value)
-  })
+  if (id === 'error') callback('error')
+  else {
+    get('thing', { id: id, type: 'boardgame', stats: 2 }, result => {
+      if (!result.items || !result.items.item) callback('error')
+      else callback(result.items.item[0].statistics[0].ratings[0].average[0].$.value)
+    })
+  }
 }
